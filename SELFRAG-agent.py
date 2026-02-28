d1fbd6563eb0e4116f6b3c0648f33d51bd08dda4
SHIVANG GUPTA <shivang0721gupta@gmail.com>
2026-02-28 17:40:07 +0530

    Installing all Dependencies & Packages

---
+++ SELFRAG-agent.py
@@ -0,0 +1,17 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel, Field
+from langchain_openai import ChatOpenAI
+from langchain import hub
+from langchain_core.output_parsers import StrOutputParser
+
+from dotenv import load_dotenv
+from typing import List
+from typing_extensions import TypedDict
+
+from langgraph.graph import END, StateGraph, START
+
+from prompts import DOCUMENT_GRADER_PROMPT, HALLUCINATION_GRADER_PROMPT, ANSWER_GRADER_PROMPT
