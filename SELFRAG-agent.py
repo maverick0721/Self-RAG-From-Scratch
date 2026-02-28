69cf77a452f0a6f602e67fd459fec878f2016c69
SHIVANG GUPTA <shivang0721gupta@gmail.com>
2026-02-28 18:05:36 +0530

    Generate or Not Answer

--- SELFRAG-agent.py
+++ SELFRAG-agent.py
@@ -156,3 +156,31 @@ def grade_documents(state):
     
     state['documents'] = filtered_docs
     return state
+
+
+# FUNCTION TO DECIDE WHETHER TO GENERATE OR NOT
+def decide_to_generate(state):
+    """
+    Determines whether to generate an answer, or re-generate a question.
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        str: Binary decision for next node to call
+    """
+
+    print("---ASSESS GRADED DOCUMENTS---")
+    filtered_documents = state["documents"]
+
+    if not filtered_documents:
+        # All documents have been filtered check_relevance
+        # We will re-generate a new query
+        print(
+            "---DECISION: ALL DOCUMENTS ARE NOT RELEVANT TO QUESTION, END ---"
+        )
+        return "end"
+    else:
+        # We have relevant documents, so generate answer
+        print("---DECISION: GENERATE---")
+        return "continue"
