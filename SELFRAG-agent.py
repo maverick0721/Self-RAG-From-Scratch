8b04da914a22aa73d6e288b847a1607d51dde092
SHIVANG GUPTA <shivang0721gupta@gmail.com>
2026-02-28 17:51:14 +0530

    Created DataModels for Grading

--- SELFRAG-agent.py
+++ SELFRAG-agent.py
@@ -44,4 +44,26 @@ class GraphState(TypedDict):
     hallucinated: bool
     valid_answer: bool
 
-    
+# DATAMODEL FOR GRADING DOCUMENTS
+class GradeDocuments(BaseModel):
+    """Binary score for relevance check on retrieved documents."""
+
+    binary_score: str = Field(
+        description="Documents are relevant to the question, 'yes' or 'no'"
+    )
+
+# DATAMODEL FOR GRADING HALLUCINATIONS
+class GradeHallucinations(BaseModel):
+    """Binary score for hallucination present in generation answer."""
+
+    binary_score: str = Field(
+        description="Answer is grounded in the facts, 'yes' or 'no'"
+    )
+
+# DATAMODEL FOR GRADING THE FINAL ANSWER
+class GradeAnswer(BaseModel):
+    """Binary score to assess answer addresses question."""
+
+    binary_score: str = Field(
+        description="Answer addresses the question, 'yes' or 'no'"
+    )   
